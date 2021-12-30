b07bca402f23fe2002048072d7992bce1a6c9e4e
Prakash-Reddy08 <mjprakashreddy1@gmail.com>
2021-12-30 20:30:10 +0530

    changes

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-console.log(process.env.REACT_APP_API_ENDPOINT);
-
 const baseUrl = axios.create({
     baseURL: `${process.env.REACT_APP_API_ENDPOINT || `http://localhost:5000/api`}`
 })
